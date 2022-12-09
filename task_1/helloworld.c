41acf57a1cb1872b666de69adccc428cde1cc79e
George Levinson <gosha.levinson@gmail.com>
2022-12-09 15:24:40 +0200

    Added .gitignore to the repo. Completed task one: created console app to send specified data to stdout. Local execution result provided in a separate file.

---
+++ task_1/helloworld.c
@@ -0,0 +1,10 @@
+static const char group[] = "G#4";
+static const char email[] = "gosha.levinson@gmail.com";
+static const char name[] = "George Levinson";
+static const char repo[] = "https://github.com/georgelevinson/glBaseCamp.git";
+static const char shared_drive[] = "https://drive.google.com/drive/folders/10WBtqL-pOZ9e1sNbUwDn72BpRSsNGxQe?usp=share_link";
+
+int main(void)
+{
+    printf("\n\nName: %s\nGroup: %s\nEmail: %s\nRepo: %s\nGoogle Drive: %s\n\n", name, group, email, repo, shared_drive);
+}
