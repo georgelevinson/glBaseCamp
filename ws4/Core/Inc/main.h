78c12ccbd20b079fbc6fd35a3d6a765e767de20e
George Levinson <gosha.levinson@gmail.com>
2023-03-18 20:18:14 +0200

    Created ws4 proj. Implemented LED pin toggle via GPIO interrupt. Implemtented interrupt-based two way communication via UART.

---
+++ ws4/Core/Inc/main.h
@@ -0,0 +1,82 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+#define SWT1_PIN		GPIO_PIN_11
+#define SWT2_PIN		GPIO_PIN_15
+#define SWT3_PIN		GPIO_PIN_9
+#define SWT4_PIN		GPIO_PIN_6
+#define SWT5_PIN		GPIO_PIN_8
+
+#define GREEN_LED		GPIO_PIN_12
+#define ORANGE_LED 		GPIO_PIN_13
+#define RED_LED 		GPIO_PIN_14
+#define BLUE_LED		GPIO_PIN_15
+
+#define OUT_BUFFER_LEN  38
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
